fa63095abf21574f3895664a63596154f08a6700
Educate <3036664758@qq.com>
2020-03-20 18:05:33 +0800

    构建项目，配置全局样式及路由，element-ui

---
+++ vue.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+module.exports = {
+//基本路径
+publicPath: process.env.NODE_ENV === 'production' ? ' ' : '/',
+//1/输出文件目录
+outputDir: process .env .NODE_ENV === 'production' ? 'dist' : 'devdist',
+// eslint-loader 是否在保存的时候检查
+lintOnSave: false,
+/**
+* webpack配置,see https://github : com/vuejs/vue-cli/blob/ dev/docs/webpack . md
+**/
+chainWebpack: (config) => {
+},
+configureWebpack: (config) => {
+// config.resolve = { //配置解析别名
+//extensions: ['.js', '.json', '.vue'],
+//
+//alias: {
+//
+//'@': path. resolve(_ _dirname, ' ./src' ),
+//
+//'public': path.resolve(__ dirname ,
+//public'),
+//
+//components': path. resolve(_ dirname, ' ./ src/ components ')
+//
+//’common': path. resolve(_ dirname, ' ./src/ common' ),
+//
+//'api': path. resolve(__ dirname, ' . /src/api'),
+//
+//'views': path. resolve(_ dirname,
+//./src/views'),
+//
+//'data' : path. resolve(_ dirname,' ./ src/data' )
+// }
+// }
+},
+//生产环境是否生成sourceMap 文件
+productionSourceMap: false,
+// css相关配置
+css: {
+// 是否使用css分离插件ExtractTextPlugin
+extract: true,
+//开启CSS source maps?
+sourceMap: false,
+// css预设器配置项
+loaderOptions: {
+    sass:{
+         prependData: '@import "./src/styles/main.scss";'
+    }
+   
+},
+//启用CSS modules for all Css / pre-processor files .
+modules: false
+},
+// use thread-loader for babel & TS in production build
+// enabled by default if the machine has more than 1 cores
+parallel: require('os').cpus().length > 1,
+/**
+PWA插件相关配置,see https: //github . com/ vuejs/vue-cli/ tree/ dev/packages/%48vue/cli-
+*/
+pwa: {},
+// webpack- dev-server 相关配置
+devServer: {
+    open: false, // 编译完成是否打开网页
+    host: '0.0.0.0', //  1指定使用地址，默认localhost,e. 0.0.0代表可以被外界访问
+    port: 8081, //访问端口
+    https: false, //编译失败时刷新页面
+    hot: true, //开启热加载
+    hotOnly: false,
+    proxy: null, //设置代理
+    overlay: { //1全屏模式下是否显示脚本错误
+    warnings: true,
+    errors: true
+    },
+    before: app => {
+
+    }
+},
+//第三方插件配置
+pluginOptions: {
+
+}
+}
